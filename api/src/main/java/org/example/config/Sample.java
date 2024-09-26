a970c88d309d4425712738c02b7ec2cd7fa34174
MFORCE\takuya_nakai <takuya_nakai@mforce.co.jp>
2024-09-26 16:26:20 +0900

    a

--- api/src/main/java/org/example/config/Sample.java
+++ api/src/main/java/org/example/config/Sample.java
@@ -1,20 +1,41 @@
 package org.example.config;
 
+import org.apache.logging.log4j.util.Supplier;
 import org.example.function.Method1;
 import org.example.function.Method2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Sample {
+
+    private final Logger logger = LoggerFactory.getLogger(Sample.class);
     
+    // @Bean
+    // Method1 method1() {
+    //     return new Method1();
+    // }
+
+    // @Bean
+    // Method2 method2() {
+    //     return new Method2();
+    // }
+
     @Bean
-    Method1 method1() {
-        return new Method1();
+    public Supplier<Void> method1() {
+        return () -> {
+            System.out.println("hello world from method1");
+            return null;
+        };
     }
 
     @Bean
-    Method2 method2() {
-        return new Method2();
+    public Supplier<Void> method2() {
+        return () -> {
+            System.out.println("hello world from method2");
+            return null;
+        };
     }
 }
