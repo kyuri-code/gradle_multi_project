5c9983cec7a4e0711b5ea172df803f075aed0734
MFORCE\takuya_nakai <takuya_nakai@mforce.co.jp>
2024-09-26 16:04:54 +0900

    a

--- api/src/main/java/org/example/Main.java
+++ api/src/main/java/org/example/Main.java
@@ -1,15 +1,19 @@
 package org.example;
 
 import org.example.config.AppConfig;
+import org.example.config.Sample;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Import;
 
-@Import(AppConfig.class)
-@SpringBootConfiguration
-@EnableAutoConfiguration
+// @Import(AppConfig.class)
+// @SpringBootConfiguration
+// @EnableAutoConfiguration
+@Import(Sample.class)
+@SpringBootApplication
 public class Main {
 
     public static void main(final String[] args) {
