50c27fa8e0ba948567a281a58cbe2ea89e1afc87
MFORCE\takuya_nakai <takuya_nakai@mforce.co.jp>
2024-09-27 17:17:24 +0900

    add logic for connection db

--- api/src/main/java/org/example/service/DatabaseQueryServicve.java
+++ api/src/main/java/org/example/service/DatabaseQueryServicve.java
@@ -1,31 +1,82 @@
 package org.example.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+
 @Service
 public class DatabaseQueryServicve {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final String SECRET_NAME = "dev/si-dev-pgsql153/super-user";
 
-    public DatabaseQueryServicve(JdbcTemplate jdbcTemplate){
-        this.jdbcTemplate = jdbcTemplate;
-    }
+    private final Region REGION = Region.of("ap-northeast-1");
 
+    private final String SQL = "SELECT * FROM sample_table";
+
+    private final String DBNAME = "sample";
+
+    @SuppressWarnings("unchecked")
     public List<String> getDataFromDatabase() {
-        String sql = "SELECT * FROM sample_table";
+        
+        SecretsManagerClient client = SecretsManagerClient.builder()
+                .region(REGION)
+                .build();
+        
+        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                .secretId(SECRET_NAME)
+                .build();
 
-        return jdbcTemplate.query(sql, new RowMapper<String>() {
-            @Override
-            public String mapRow(@SuppressWarnings("null") ResultSet rs, int rowNum) throws SQLException {
-                return rs.getString("name");
-            }
-        });
+        GetSecretValueResponse getSecretValueResponse;
+
+        try {
+            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+        } catch (Exception e) {
+            throw e;
+        }
+
+        String secret = getSecretValueResponse.secretString();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String,String> secretMap;
+
+        try {
+            secretMap = objectMapper.readValue(secret, Map.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to parse secret from secrets manager", e);
+        }
+
+        String host = secretMap.get("host");
+        String port = secretMap.get("port");
+        String username = secretMap.get("username");
+        String password = secretMap.get("password");
+
+        String url = "jdbc:postgresql://" + host + ":" + port + "/" + DBNAME;
+
+        try(Connection connection = DriverManager.getConnection(url, username, password)) {
+            JdbcTemplate jdbcTemplate = new JdbcTemplate();
+            return jdbcTemplate.query(SQL, new RowMapper<String>() {
+                @Override
+                public String mapRow(@SuppressWarnings("null") ResultSet rs, int rowNum) throws SQLException {
+                    return rs.getString("name");
+                }
+            });
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to connect ot the database or execute query", e);
+        }
     }
 
 }
